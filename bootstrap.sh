1ec19656d1688c2ec1d690a0da065508a275768f
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-06-11 10:31:11 -0430

    add exports to separate files and include aliases

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,34 +2,48 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
+echo "----------> Provinsing process started..."
+echo ""
 echo "----------> Installing Homebrew..."
+echo ""
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
+echo ""
 echo "----------> Installing Homebrew Apps..."
 brew bundle
 
+echo ""
 echo "----------> Installing oh-my-zsh..."
 curl -L http://install.ohmyz.sh | sh
 
+echo ""
 echo "----------> Linking dotfiles configuration..."
 rcup
 
+echo ""
 echo "----------> Reloading ZSH config"
 source ~/.zshrc
 
+echo ""
 echo "----------> Changing shell to ZSH..."
 chsh -s $(which zsh)
 
+echo ""
 echo "----------> Installing Homebrew Casks..."
 brew bundle Caskfile
 
+echo ""
 echo "----------> Installing Atom packages..."
 apm stars --user jonathanwiesel --install
 
+echo ""
 echo "----------> Installing NPM global modules..."
 npm install -g bower express forever grunt-cli meanio nodemon node-inspector yo
 
+echo ""
 echo "----------> Setting up OS X configuration..."
 source .osx
 
+echo ""
+echo ""
 echo "----------> Provisioning process complete."
--- zshrc
+++ zshrc
@@ -14,8 +14,8 @@ source $ZSH/oh-my-zsh.sh
 # search for rbenv
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
 
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-export EDITOR="atom --wait"
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-export PATH="/usr/local/heroku/bin:/usr/local/bin:$PATH"
+# exports
+[[ -f ~/.exports ]] && source ~/.exports
+
+# aliases
+[[ -f ~/.aliases ]] && source ~/.aliases
