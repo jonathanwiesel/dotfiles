b66c5cfee5c94fb905129f2bad73f6a83b21d578
Jonathan Wiesel <jonathanwiesel@gmail.com>
2016-04-27 22:02:20 +0200

    :star: Add better git diff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,7 +9,7 @@ alias gch="git checkout"
 alias gb="git branch"
 alias gr="git remote"
 alias gl="git log --oneline --decorate --all --graph"
-alias gd="git diff"
+alias gd="git diff --color | diff-so-fancy"
 alias gdt="git difftool"
 
 # Vagrant aliases
