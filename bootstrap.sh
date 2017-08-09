2c61fe4fc1eed655abc8ca7bf7972a93a8b60e6f
Jonathan Wiesel <jonathanwiesel@gmail.com>
2017-08-09 19:50:01 +0200

    :scissors: Remove unnecesary default npm modules

--- bootstrap.sh
+++ bootstrap.sh
@@ -101,7 +101,7 @@ if hash npm 2>/dev/null; then
     echo
     echo "----------> Installing NPM global modules..."
     echo
-    npm install -g awm bower express-generator forever grunt-cli mean-cli nativefier nodemon node-inspector sails slap yo
+    npm install -g bower express-generator grunt-cli nativefier slap
   else
     echo "----------> Skipping NPM global modules..."
   fi
