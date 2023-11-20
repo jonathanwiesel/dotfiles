6458b44b449d1d1e452e773181c1b03d79e2d61b
Jonathan Wiesel <jonathan_wiesel@mckinsey.com>
2023-11-20 16:26:06 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,7 +36,7 @@ if hash brew 2>/dev/null; then
 else
     echo "----------> Installing Homebrew..."
     echo
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install)"
 
     echo
     brew doctor
